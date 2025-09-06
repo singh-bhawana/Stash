879382657437400e2080f1b0ef0e6e95e3a65cbe
ikanikab <ikanikabhardwaj18@gmail.com>
2025-09-07 02:52:06 +0530

    ids resources added

--- src/data/resources.js
+++ src/data/resources.js
@@ -30,16 +30,16 @@ export const resources = {
     },
       ids: {
         notes: [
-          { name: "Common NumPy Array Functions", url: "/resources/ECE/IDS/notes/Common NumPy Array Functions.pdf" },
-          { name: "Data Manipulation with Pandas", url: "/resources/ECE/IDS/notes/Data Manipulation in Python using Pandas.pdf" },
-          { name: "Data Science Unit 1", url: "/resources/ECE/IDS/notes/data science UNIT 1.docx" },
-          { name: "Data Wrangling", url: "/resources/ECE/IDS/notes/Data Wrangling.pdf" },
-          { name: "NumPy Arithmetic Operations", url: "/resources/ECE/IDS/notes/NumPy Arithmetic Array Operations.pdf" },
-          { name: "NumPy Array Indexing", url: "/resources/ECE/IDS/notes/Numpy Array Indexing.pdf" },
-          { name: "NumPy Array Slicing", url: "/resources/ECE/IDS/notes/NumPy Array Slicing Notes.pdf" },
-          { name: "Pandas Notes", url: "/resources/ECE/IDS/notes/pandas.docx" },
-          { name: "Tableau Unit 4", url: "/resources/ECE/IDS/notes/TABLEAU UNIT 4.docx" },
-          { name: "Unit 2-1", url: "/resources/ECE/IDS/notes/UNIT 2-1.docx" },
+          { name: "Common NumPy Array Functions", url: "/resources/ids/notes/Common NumPy Array Functions.pdf" },
+          { name: "Data Manipulation with Pandas", url: "/resources/ids/notes/Data Manipulation in Python using Pandas.pdf" },
+          { name: "Data Science Unit 1", url: "/resources/ids/notes/data science UNIT 1.docx" },
+          { name: "Data Wrangling", url: "/resources/ids/notes/Data Wrangling.pdf" },
+          { name: "NumPy Arithmetic Operations", url: "/resources/ids/notes/NumPy Arithmetic Array Operations.pdf" },
+          { name: "NumPy Array Indexing", url: "/resources/ids/notes/Numpy Array Indexing.pdf" },
+          { name: "NumPy Array Slicing", url: "/resources/ids/notes/NumPy Array Slicing Notes.pdf" },
+          { name: "Pandas Notes", url: "/resources/ids/notes/pandas.docx" },
+          { name: "Tableau Unit 4", url: "/resources/ids/notes/TABLEAU UNIT 4.docx" },
+          { name: "Unit 2-1", url: "/resources/ids/notes/UNIT 2-1.docx" },
         ],
         pyqs: [],
         books: [],
@@ -277,6 +277,24 @@ export const resources = {
         { name: "CS Complete Notes", url: "/resources/communication/notes/CS complete notes.pdf" }
       ]
     },
+
+    ids: {
+        notes: [
+          { name: "Common NumPy Array Functions", url: "/resources/ids/notes/Common NumPy Array Functions.pdf" },
+          { name: "Data Manipulation with Pandas", url: "/resources/ids/notes/Data Manipulation in Python using Pandas.pdf" },
+          { name: "Data Science Unit 1", url: "/resources/ids/notes/data science UNIT 1.docx" },
+          { name: "Data Wrangling", url: "/resources/ids/notes/Data Wrangling.pdf" },
+          { name: "NumPy Arithmetic Operations", url: "/resources/ids/notes/NumPy Arithmetic Array Operations.pdf" },
+          { name: "NumPy Array Indexing", url: "/resources/ids/notes/Numpy Array Indexing.pdf" },
+          { name: "NumPy Array Slicing", url: "/resources/ids/notes/NumPy Array Slicing Notes.pdf" },
+          { name: "Pandas Notes", url: "/resources/ids/notes/pandas.docx" },
+          { name: "Tableau Unit 4", url: "/resources/ids/notes/TABLEAU UNIT 4.docx" },
+          { name: "Unit 2-1", url: "/resources/ids/notes/UNIT 2-1.docx" },
+        ],
+        pyqs: [],
+        books: [],
+      },
+      
     },
   };
   
